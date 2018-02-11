fea360db970a135ead7fad8719cb3613e39bbe8a
Zatsune No Mokou <contact@zatsunenomokou.eu>
2018-02-11 21:56:25 +0100

    Change npm scripts into js ones

--- webextension/data/js/classes/ZDK.js
+++ webextension/data/js/classes/ZDK.js
@@ -363,6 +363,30 @@ class ZDK{
 		// Return true is the event haven't been canceled
 		return node.dispatchEvent(evt);
 	}
+
+	/**
+	 *
+	 * @param {Number} millisecond
+	 * @return {Promise<void>}
+	 */
+	static setTimeout(millisecond) {
+		return new Promise(resolve=>{
+			setTimeout(resolve, millisecond);
+		})
+	}
+
+	/**
+	 *
+	 * @param {Window=window} win
+	 * @return {{height: number, width: number}}
+	 */
+	static getPageSize(win=window) {
+		const doc = win.document;
+		return {
+			'height': win.innerHeight || doc.documentElement.clientHeight || doc.body.clientHeight,
+			'width': win.innerWidth || doc.documentElement.clientWidth || doc.body.clientWidth
+		}
+	}
 }
 
 var consoleMsg = ZDK.consoleMsg;
