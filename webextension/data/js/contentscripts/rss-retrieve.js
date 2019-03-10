48614446610d00d73e6f57cbc4c58c221dccc9e2
Zatsune No Mokou <contact@zatsunenomokou.eu>
2019-03-10 11:47:32 +0100

    rss link detection

--- webextension/data/js/contentscripts/rss-retrieve.js
+++ webextension/data/js/contentscripts/rss-retrieve.js
@@ -13,7 +13,11 @@
 
 
 
+	// Force creation of a new string
+	let currentHref = `${window.location.href}`;
 	const detectRssFeeds = function() {
+		currentHref = `${window.location.href}`;
+
 		rssLinks = Array.from(document.querySelectorAll('link[rel="alternate"][type]'));
 
 		rssLinks = rssLinks
@@ -26,13 +30,35 @@
 				}
 			})
 		;
+
+		rssLinks = rssLinks.concat(
+			Array.from(document.body.querySelectorAll('[href*="/rss/"],[href*="/atom/"]'))
+				.filter(function (node) {
+					return node.tagName !== 'link' || link.rel !== 'alternate'
+				})
+				.map(node => ({
+					'href': node.href,
+					'type': node.href.includes('rss') ? 'rss-link' : 'atom-link',
+					'title': (node.textContent.length === 0) ? null : node.textContent
+				}))
+		);
+
+		const uniqueHref = [];
+		rssLinks = rssLinks.filter(item => {
+			if (uniqueHref.indexOf(item.href) === -1) {
+				uniqueHref.push(item.href);
+				return true;
+			}
+
+			return false;
+		})
 	};
 
 
 
 	const onConnect = function (port) {
 		if (port.sender.id === chrome.runtime.id && port.name === 'ztoolbox_rss-retrieve') {
-			if (rssLinks === undefined) {
+			if (rssLinks === undefined || currentHref !== window.location.href) {
 				detectRssFeeds();
 			}
 
