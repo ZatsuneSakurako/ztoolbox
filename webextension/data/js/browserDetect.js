e7170a01e1211a4a441691ec98b09ecc7fef70b8
Zatsune No Mokou <contact@zatsunenomokou.eu>
2022-02-15 00:01:19 +0100

    use self as global variable access

--- webextension/data/js/browserDetect.js
+++ webextension/data/js/browserDetect.js
@@ -1,3 +1,3 @@
 // https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser/9851769#9851769
-export const isFirefox = typeof InstallTrigger !== 'undefined' || /(?:firefox|fxios)\/(\d+)/i.test(navigator.userAgent);
+export const isFirefox = typeof InstallTrigger !== 'undefined' || /(?:firefox|fxios)\/(\d+)/i.test(self.navigator.userAgent);
 export const isChrome = !self.navigator.userAgent.toLowerCase().indexOf("chrome") !== -1;
