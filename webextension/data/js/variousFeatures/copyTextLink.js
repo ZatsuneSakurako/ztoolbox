4b34aae2257e13b49e8f7cd53c309c08c0a648ce
Zatsune No Mokou <contact@zatsunenomokou.eu>
2022-10-07 18:51:13 +0200

    copyTextLink syntax fix

--- webextension/data/js/variousFeatures/copyTextLink.js
+++ webextension/data/js/variousFeatures/copyTextLink.js
@@ -7,8 +7,13 @@ import {i18ex} from "../translation-api.js";
 async function initMenuCopyTextLink() {
 	await i18ex.loadingPromise;
 
-	await browser.scripting.unregisterContentScripts(['copyTextLink']);
-	await chrome.scripting.register([
+	const currentContentScripts = new Set(await browser.scripting.getRegisteredContentScripts());
+	if (currentContentScripts.has('copyTextLink')) {
+		await browser.scripting.unregisterContentScripts({
+			ids: ['copyTextLink']
+		});
+	}
+	await chrome.scripting.registerContentScripts([
 		{
 			"id": "copyTextLink",
 			"js": [
