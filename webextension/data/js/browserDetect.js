09b277d8607b02bd580a5de433e15dbfc068cac8
Zatsune No Mokou <contact@zatsunenomokou.eu>
2022-10-26 19:08:46 +0200

    browserDetect simplify

--- webextension/data/js/browserDetect.js
+++ webextension/data/js/browserDetect.js
@@ -1,3 +1,3 @@
 // https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser/9851769#9851769
 export const isFirefox = /(?:firefox|fxios)\/(\d+)/i.test(self.navigator.userAgent);
-export const isChrome = !isFirefox && !self.navigator.userAgent.toLowerCase().indexOf("chrome") !== -1;
+export const isChrome = !isFirefox && !!self.navigator.userAgent.toLowerCase().includes("chrome");
