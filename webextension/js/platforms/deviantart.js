c8bffbea7832062e79a7055598c41cf6c83d6848
Zatsune No Mokou <ZatsuneNoMokou@users.noreply.github.com>
2023-03-28 14:45:37 +0200

    change deviantart icon url

--- webextension/js/platforms/deviantart.js
+++ webextension/js/platforms/deviantart.js
@@ -4,7 +4,8 @@ import JSON5 from '../../lib/json5.js';
 const deviantArt = {
 	// Old data url dataURL:"http://www.deviantart.com/notifications/watch",
 	dataURL:"https://www.deviantart.com/watch/deviations",
-	defaultFavicon: 'https://icons.duckduckgo.com/ip2/www.deviantart.com.ico',
+	defaultFavicon: 'https://www.deviantart.com/favicon.ico',
+	// defaultFavicon: 'https://icons.duckduckgo.com/ip2/www.deviantart.com.ico',
 	getViewURL: function(websiteState) {
 		if (websiteState.count > 0) {
 			return this.dataURL;
