ffb54a2c8b7f789aefc5a6abf80a57971ce5e579
Zatsune No Mokou <ZatsuneNoMokou@users.noreply.github.com>
2023-07-15 11:24:09 +0200

    settings - remove js rendering

--- webextension/js/options-data.js
+++ webextension/js/options-data.js
@@ -13,7 +13,6 @@ export const options = {
 	"tabPageServerIp_alias": {
 		"type": "json",
 		"value": "{\"127.0.0.1\": \"localhost\"}",
-		"group": "featurePreference",
 		"sync": true
 	},
 	/*			Theme			*/
