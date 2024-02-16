da9447c587c43315628778286e6327ac3b7db69a
Zatsune No Mokou <ZatsuneNoMokou@users.noreply.github.com>
2024-02-16 22:19:21 +0100

    page meta rating

--- webextension/js/variousFeatures/loadPageDatas.js
+++ webextension/js/variousFeatures/loadPageDatas.js
@@ -1,12 +1,26 @@
-export async function loadTabOpenGraph(activeTab) {
+export async function loadPageDatas(activeTab) {
 	return await chrome.scripting.executeScript({
 		target: {
 			tabId: typeof activeTab === 'object' ? activeTab.id : activeTab,
 		},
-		files: [
-			'/js/contentscripts/openGraphData.js'
-		],
-		injectImmediately: true
+		injectImmediately: false,
+		world: 'MAIN',
+		func: function () {
+			return {
+				openGraphData: Object.fromEntries(
+					new Map([...
+						document.querySelectorAll('meta[property^="og:"]')]
+						.map(node => {
+							return [
+								node.getAttribute('property')?.substring(3),
+								node.content
+							]
+						})
+					)
+				),
+				rating: [...document.querySelectorAll('meta[name="rating" i]')].map(n => n.getAttribute('content'))
+			}
+		}
 	})
 		.catch(console.error)
 	;
