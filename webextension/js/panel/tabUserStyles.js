3e06f855b8320d6435e687dc3d3539852194d8aa
Zatsune No Mokou <ZatsuneNoMokou@users.noreply.github.com>
2025-06-03 21:56:26 +0200

    userscript - firefox does not support onConnect, maybe later

--- webextension/js/panel/tabUserStyles.js
+++ webextension/js/panel/tabUserStyles.js
@@ -200,23 +200,11 @@ async function userScriptSendEvent(eventName, target, tab) {
 	if (!tab) tab = await getCurrentTab();
 	console.info('Sending to tab ', tab, 'the event', eventName, 'for ', target);
 
-	const port = chrome.tabs.connect(tab.id);
-	try {
-		await port.postMessage({
-			type: "userScriptEvent",
-			target,
-			eventName: `menuCommand-${eventName}`,
-		});
-	} catch (e) {
-		console.error(e);
-	} finally {
-		port.disconnect();
-	}
-	/*await chrome.tabs.sendMessage(tab.id, {
+	await chrome.tabs.sendMessage(tab.id, {
 		type: "userScriptEvent",
 		target,
 		eventName: `menuCommand-${eventName}`,
-	});*/
+	});
 }
 
 /**
