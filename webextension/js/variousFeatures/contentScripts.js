c93490e0df2135a49ae56d83e8e787047d233fbc
Zatsune Sakurako <ZatsuneSakurako@users.noreply.github.com>
2026-05-24 21:23:42 +0200

    run "panel" runAt script when userScript.execute is not available

--- webextension/js/variousFeatures/contentScripts.js
+++ webextension/js/variousFeatures/contentScripts.js
@@ -1,4 +1,4 @@
-import {_userScriptsStateStoreKey, _userScriptsStoreKey} from "../constants.js";
+import {_userScriptsStateStoreKey, _userScriptsStoreKey, userScriptPanelDisabled} from "../constants.js";
 import {contentStyles} from "./contentStyles.js";
 import {getBasicNotificationOptions} from "./contentScripts/chrome-notification.js";
 import {getUserscriptData, setUserscriptData, writeClipboard} from "../classes/chrome-native.js";
@@ -1150,7 +1150,14 @@ class ContentScripts {
 
 		for (let userScript of userScripts) {
 			const enabled = this.userScriptStates[userScript.fileName] ?? userScript.enabled;
-			if (!enabled || userScript.runAt === 'panel') continue;
+			if (!enabled) continue;
+			if (userScript.runAt === 'panel') {
+				if (userScriptPanelDisabled) {
+					console.warn(`UserScript ${userScript.fileName} runAt is "panel" but this mode is unavailable. Enabling it anyway.`);
+				} else {
+					continue;
+				}
+			}
 			userScriptIds.add(userScript.fileName);
 
 			const registrationUserScript = this.#userScriptToRegistrationOptions(userScript);
