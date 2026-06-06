70aa5ab7bfac4a0c7bb49e568c8ca281310721e6
Zatsune Sakurako <ZatsuneSakurako@users.noreply.github.com>
2026-06-07 01:36:57 +0200

    firefox keep background page alive

--- webextension/js/firefox_keep-alive.js
+++ webextension/js/firefox_keep-alive.js
@@ -4,16 +4,14 @@ let lastTimeSent = 0;
 setInterval(() => {
 	const now = Date.now();
 
-	// Only send it if at least 20 seconds passed since last SEND (not last receive)
-	if (now - lastTimeSent < 20_000) return;
+	// Only send it if at least 25 seconds passed since last SEND (not last receive)
+	if (now - lastTimeSent < 25_000) return;
 
 	chrome.runtime.sendMessage({
 		type: "HEARTBEAT",
-		data: {
-			[Math.random().toString(36)]: new Date().getTime(),
-		}
+		data: { [Math.random().toString(36)]: new Date().getTime() }
 	})
 		.then(() => {
 			lastTimeSent = now;
 		});
-}, 1_000); // Run the check every second, but only ACT every 5 seconds
+}, 5_000); // Run the check every second, but only ACT every 5 seconds
